3096a13a005b60e2cf237bfde8aee0d15617adcd
mohmawoed <37363762+mohmawoed@users.noreply.github.com>
2024-01-31 11:14:03 +0200

    Update buildImage.groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -3,8 +3,8 @@
 def call() {
     echo "Building images."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t  mohmawoed/demo-app:jma2.0 .'
+        sh 'docker build -t  mohmawoed/demo-app:jma1.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push mohmawoed/demo-app:jma2.0'
+        sh 'docker push mohmawoed/demo-app:jma1.0'
     }
 }
